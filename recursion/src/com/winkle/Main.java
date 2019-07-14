cf4c5dc33965605f7b7541da6f040d07c76d2eba
jwin4740@gmail.com <jwin4740@gmail.com>
2019-07-14 15:45:51 -0400

    hex working

--- recursion/src/com/winkle/Main.java
+++ recursion/src/com/winkle/Main.java
@@ -46,29 +46,49 @@ Module - Code design and Implementation Assignment
 
 package com.winkle;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
+
+    static final int A = 10;
+    static final int B = 11;
+    static final int C = 12;
+    static final int D = 13;
+    static final int E = 14;
+    static final int F = 15;
+    static final HashMap<Character, Integer> hexMap = new HashMap<>() {
+        {
+            put('A', 10);
+            put('B', 11);
+            put('C', 12);
+            put('D', 13);
+            put('E', 14);
+            put('F', 15);
+        }
+    };
+    static int hexLength = 0;
 
 
     public static void main(String[] args) {
 
         Scanner s = new Scanner(System.in);
 
-        System.out.println("Enter a binary number:");
+//        System.out.println("Enter a binary number:");
+//        String start = s.nextLine();
+//        int finalVal = bin2Dec(start);
+//        System.out.println(finalVal);
+        System.out.println("Enter a hex number:");
         String start = s.nextLine();
-        int finalVal = bin2Dec(start);
+        int finalVal = hex2Dec(start);
         System.out.println(finalVal);
     }
 
-//        bin2Dec("10");
 
-    //    Write a recursive method that parses a hex number as a string into a decimal integer
     public static int bin2Dec(String binaryString) {
         return bin2Dec(binaryString, 0);
     }
 
-    // test case "1111
     public static int bin2Dec(String binaryString, int index) {
         // length of binary string
         int leng = binaryString.length();
@@ -89,5 +109,37 @@ public class Main {
 
     }
 
+    //    Write a recursive method that parses a hex number as a string into a decimal integer
+    public static int hex2Dec(String hexString) {
 
+        return hex2Dec(hexString, 0);
+    }
+
+    public static int hex2Dec(String hexString, int index) {
+//        StringBuilder sb = new StringBuilder(hexString);
+//        String myChar = hexString.substring(0, index + 1);
+        int leng = hexString.length();
+        int value;
+        int val;
+        char currentCharacter = hexString.charAt(index);
+        if (Character.isLetter(currentCharacter)) {
+            val = hexMap.get(currentCharacter);
+        } else {
+            val = Character.getNumericValue(hexString.charAt(index));
+        }
+
+
+        if (index == leng - 1) {
+            return (val > 0) ? val : 0;
+        }
+
+
+        int adder = val > 0 ? val * ((int) Math.pow(16, leng - index - 1)) : 0;
+        int newIndex = index + 1;
+        value = +adder + hex2Dec(hexString, newIndex);
+
+
+
+        return value;
+    }
 }
--- recursion/src/com/winkle/MainTest.java
+++ recursion/src/com/winkle/MainTest.java
@@ -1,14 +1,22 @@
 package com.winkle;
 
+import org.junit.Test;
+
+
 import static org.junit.Assert.*;
 
 public class MainTest {
 
-    @org.junit.Test
-    public void bin2Dec() {
-        String testBin = "11";
 
-//        assertEquals(3, );
+    @Test
+    public void hex2Dec() {
+        assertEquals(837, Main.hex2Dec("345"));
+        assertEquals(0, Main.hex2Dec("0"));
+
+        assertEquals(1029211, Main.hex2Dec("FB45B"));
 
     }
+
+
 }
+
